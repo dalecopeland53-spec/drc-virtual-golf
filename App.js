1da27fab4683703aada11e9dfc033e5892f4beeb
dalecopeland53-spec <dale.copeland53@gmail.com>
2026-09-06 16:33:43 +1000

    Refactor App.js to update live project URL

--- App.js
+++ App.js
@@ -1,33 +1,48 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
+
 import { WebView } from 'react-native-webview';
 
 export default function App() {
-  // Points directly to your successfully deployed live site url
-  const liveProjectUrl = 'https://github.io';
+
+  const liveProjectUrl =
+    'https://dalecopeland53-spec.github.io/drc-virtual-golf/';
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#03081e" />
-      <WebView 
-        source={{ uri: liveProjectUrl }} 
+
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#03081e"
+      />
+
+      <WebView
+        source={{ uri: liveProjectUrl }}
         style={styles.webview}
         javaScriptEnabled={true}
         domStorageEnabled={true}
         startInLoadingState={true}
         scalesPageToFit={true}
       />
+
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#03081e',
   },
+
   webview: {
     flex: 1,
     backgroundColor: '#03081e',
   },
+
 });
