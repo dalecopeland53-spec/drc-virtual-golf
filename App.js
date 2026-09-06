7e3454dec87b7d33b8c1333930dccf513641cf3c
dalecopeland53-spec <dale.copeland53@gmail.com>
2026-09-06 17:32:43 +1000

    Implement microphone permission request on Android
    Added microphone permission request for Android users.

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import {
   SafeAreaView,
   StyleSheet,
   StatusBar,
+  Platform,
+  PermissionsAndroid,
 } from 'react-native';
 
 import { WebView } from 'react-native-webview';
 
 export default function App() {
-
   const liveProjectUrl =
     'https://dalecopeland53-spec.github.io/drc-virtual-golf/';
 
+  useEffect(() => {
+    const requestMicrophonePermission = async () => {
+      if (Platform.OS !== 'android') return;
+
+      try {
+        await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+          {
+            title: 'DRC Virtual Golf Microphone',
+            message:
+              'DRC Virtual Golf needs microphone access so you can speak to your caddie.',
+            buttonPositive: 'Allow',
+            buttonNegative: 'Not now',
+          }
+        );
+      } catch (error) {
+        console.log('Microphone permission error:', error);
+      }
+    };
+
+    requestMicrophonePermission();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-
       <StatusBar
         barStyle="light-content"
         backgroundColor="#03081e"
@@ -26,15 +50,15 @@ export default function App() {
         javaScriptEnabled={true}
         domStorageEnabled={true}
         startInLoadingState={true}
-        scalesPageToFit={true}
+        mediaPlaybackRequiresUserAction={false}
+        allowsInlineMediaPlayback={true}
+        mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
       />
-
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: '#03081e',
@@ -44,5 +68,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#03081e',
   },
-
 });
