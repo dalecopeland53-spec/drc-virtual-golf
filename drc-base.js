4791324c26cce2655ee0a82fbc851a309701a2c2
dalecopeland53-spec <dale.copeland53@gmail.com>
2026-09-07 02:33:00 +1000

    Add conversion constant and unit mode retrieval

--- drc-base.js
+++ drc-base.js
@@ -1,1 +1,2 @@
-const $=id=>document.getElementById(id),$$=s=>document.querySelectorAll(s);
+const $=id=>document.getElementById(id),$$=s=>document.querySelectorAll(s),YARDS_TO_METRES=.9144;
+let unitMode=localStorage.getItem("drcUnitMode")||"metric";
