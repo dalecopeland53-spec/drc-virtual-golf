e2d81212decac6e767b37d81e20af15df7e16e09
dalecopeland53-spec <dale.copeland53@gmail.com>
2026-09-09 03:45:50 +1000

    Load activated approved screen set

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const commandSentRef=useRef(false);
 const webReadyRef=useRef(false);
 const pendingCommandRef=useRef('');
 
-const liveProjectUrl='https://dalecopeland53-spec.github.io/drc-virtual-golf/?build=20260908';
+const liveProjectUrl='https://dalecopeland53-spec.github.io/drc-virtual-golf/?build=20260909a';
 
 const sendToWeb=code=>{
 try{
