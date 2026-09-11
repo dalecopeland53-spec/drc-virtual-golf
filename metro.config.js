2c5baa157febc931a3645ec9dc815017e8cbf2e0
dalecopeland53-spec <dale.copeland53@gmail.com>
2026-09-11 14:43:19 +1000

    Remove obsolete Metro override

--- metro.config.js
+++
@@ -1,4 +0,0 @@
-const{getDefaultConfig}=require('expo/metro-config');
-const config=getDefaultConfig(__dirname);
-config.resolver.assetExts=[...config.resolver.assetExts,'bin'];
-module.exports=config;
